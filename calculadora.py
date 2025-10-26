2ae24becb08ed57862d0136181767a037cff68b6
joaolucas <joaolucascavalcantemartins2015@gmail.com>
2025-10-26 17:58:15 -0300

    Implementa funções básicas da calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,14 @@
 def somar(a, b):
  return a + b
+def subtrair(a,b):
+ return a-b
+def multiplicar(a,b):
+ return a*b
+def dividir(a,b):
+ return a/b
+
+
+
 print("Soma:", somar(10, 5))
 print("Subtração:", subtrair(10, 5))
 print("Multiplicação:", multiplicar(10, 5))
